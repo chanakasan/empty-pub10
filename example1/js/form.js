a90a47b73ca2e1ec08319d98dbc8bd2f5a98c17c
Chanaka Sandaruwan <chanakasan@gmail.com>
2013-06-17 12:06:18 +0530

    Create different forms for journey duration types, Add colorbox jquery plugin

--- example1/js/form.js
+++ example1/js/form.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+// jquery chosen
     var config = {
         '.chzn-select'           : {},
     '.chzn-select-deselect'  : {allow_single_deselect:true},
@@ -6,8 +7,10 @@ $(document).ready(function(){
     '.chzn-select-no-results': {no_results_text:'Oops, nothing found!'},
     '.chzn-select-width'     : {width:"95%"}
     }
-
     for (var selector in config) {
         $(selector).chosen(config[selector]);
     }
+
+// jquery colorbox
+    $(".iframe").colorbox({iframe:true, width:"60%", height:"100%"});
 });
