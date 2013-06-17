edfcd5b741ba202145d617f2ad345dd298fe37f8
Chanaka Sandaruwan <chanakasan@gmail.com>
2013-06-17 10:50:15 +0530

    Initial commit

---
+++ example1/js/form.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    var config = {
+        '.chzn-select'           : {},
+    '.chzn-select-deselect'  : {allow_single_deselect:true},
+    '.chzn-select-no-single' : {disable_search_threshold:10},
+    '.chzn-select-no-results': {no_results_text:'Oops, nothing found!'},
+    '.chzn-select-width'     : {width:"95%"}
+    }
+
+    for (var selector in config) {
+        $(selector).chosen(config[selector]);
+    }
+});
