0cd076d1c3a3ee68c60b2d8538be86083da4f56a
dream89 <chanakasan@gmail.com>
2013-07-29 12:22:11 +0530

    Move files, Update: start.html

--- index.php
+++ index.php
@@ -5,16 +5,9 @@ ini_set('error_reporting', E_ALL | E_STRICT);
 require_once 'vendor/Twig/Autoloader.php';
 Twig_Autoloader::register();
 
-$loader = new Twig_Loader_String();
-$twig = new Twig_Environment($loader);
-echo $twig->render('Hello {{ name }}!', array('name' => 'world'));
-
 /* load template */
-//include_once 'index.html.twig';
 
-$loader = new Twig_Loader_Filesystem(__DIR__.'/layouts');
-$twig = new Twig_Environment($loader, array(
-    'cache' => __DIR__.'/cache',
-));
+$loader = new Twig_Loader_Filesystem(__DIR__.'/views/request');
+$twig = new Twig_Environment($loader);
 
-echo $twig->render('admin.html', array('name' => 'Fabien'));
+echo $twig->render('start.html');
